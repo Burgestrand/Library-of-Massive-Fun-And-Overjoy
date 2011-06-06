06500ac6113a4aa6d366d2e1c5cc43b869c2b192
Kim Burgestrand <kim@burgestrand.se>
2011-06-06 14:08:39 +0200

    Add callback-waiting thread to LMFAO_ext

--- ext/lmfao_ext.c
+++ ext/lmfao_ext.c
@@ -1,6 +1,7 @@
 #include <ruby.h>
 #include <pthread.h>
 #include "lmfao.h"
+#include "lmfao_ext.h"
 
 static VALUE mLMFAO_call_nogvl(void *data);
 void *lmfao_callback(void *data);
@@ -68,6 +69,8 @@ static VALUE mLMFAO_call(VALUE self, VALUE data)
   rb_ary_push(args, rb_block_proc());
   rb_ary_push(args, data);
 
+  /* this is just so we can call LMFAO function and still allow other ruby threads to run
+   * while we wait for a result*/
   return rb_thread_blocking_region(mLMFAO_call_nogvl, (void *) args, NULL, NULL);
 }
 
@@ -90,11 +93,125 @@ void *lmfao_callback(void *data)
   return (void *) Qfalse;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+ * Our special Ruby event-listening thread functions
+ * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/*
+   Executed for each callback notification; what we receive
+   are the callback parameters. The job of this method is to:
+
+   1. Convert callback parameters into Ruby values
+   2. Call the appropriate callback with said parameters
+   3. Convert the Ruby return value into a C value
+   4. Hand over the C value to the C callback
+*/
+static VALUE LMFAO_handle_callback(void *cb)
+{
+  return Qnil;
+}
+
+typedef struct callback_waiting_t callback_waiting_t;
+struct callback_waiting_t {
+  callback_t *callback;
+  bool abort;
+};
+
+/*
+   Wait for global callback queue to contain something.
+
+   This function is called while not holding the GIL, so it’s safe
+   to do long waits in here; other threads will still run.
+
+   The job of this function is merely to wait until the callback queue
+   contains something. Once that happens, we remove the item from the
+   queue and return it to our caller through waiting->callback.
+*/
+static VALUE wait_for_callback_signal(void *w)
+{
+  callback_waiting_t *waiting = (callback_waiting_t*) w;
+
+  pthread_mutex_lock(&g_callback_mutex);
+
+  // abort signal is used when ruby wants us to stop waiting
+  while (waiting->abort == false && (waiting->callback = g_callback_queue_pop()) == NULL)
+  {
+    pthread_cond_wait(&g_callback_cond, &g_callback_mutex);
+  }
+
+  pthread_mutex_unlock(&g_callback_mutex);
+
+  return Qnil;
+}
+
+/*
+   Stop waiting for callback notification. This function
+   is invoked by Ruby when she wants us to exit.
+
+   As `wait_for_callback_signal` function is executed without holding
+   the GIL, it can potentially take forever. However, when somebody wants
+   to quit the program (for example if somebody presses CTRL-C to exit)
+   we must tell Ruby how to wake up the `wait_for_callback_signal` function
+   so Ruby can exit properly.
+*/
+static void stop_waiting_for_callback_signal(void *w)
+{
+  callback_waiting_t *waiting = (callback_waiting_t*) w;
+
+  pthread_mutex_lock(&g_callback_mutex);
+  waiting->abort = true;
+  pthread_cond_signal(&g_callback_cond);
+  pthread_mutex_unlock(&g_callback_mutex);
+}
+
+/*
+  This thread loops continously, waiting for callbacks to happen in C.
+  Once they do, it will handle the callback in a new thread.
+
+  The reason we use a thread for handling the callback is that the handler
+  itself might fire off more callbacks, that might need to be handled before
+  the handler returns. We can’t do that if the event thread is busy handling
+  the first callback.
+
+  Continously, we need to:
+    1. Unlock the Ruby GVL (allowing other threads to run while we wait)
+    2. Wait for a callback to fire
+    3. Spawn a new ruby thread to handle the callback
+*/
+static VALUE LMFAO_event_thread(void *unused)
+{
+  callback_waiting_t waiting = {
+    .callback = NULL,
+    .abort    = false
+  };
+
+  while (waiting.abort == false)
+  {
+    // release the GIL while waiting for a callback notification
+    rb_thread_blocking_region(wait_for_callback_signal, &waiting, stop_waiting_for_callback_signal, &waiting);
+
+    // if ruby wants us to abort, this will be NULL
+    if (waiting.callback)
+    {
+      rb_thread_create(LMFAO_handle_callback, (void *) waiting.callback);
+    }
+  }
+
+  return Qnil;
+}
+
+ /* * * * * * * * * * * * * * * * * * * * * * * * */
+
 /**
  * Ruby bindings for LMFAO C library.
+ *
+ * Requiring the LMFAO C library will also create the event thread
+ * at the same time. In reality you can wait with this until you
+ * need to have it running.
  */
 void Init_lmfao_ext()
 {
   VALUE mLMFAO = rb_define_class("LMFAO", rb_cObject);
   rb_define_singleton_method(mLMFAO, "call", mLMFAO_call, 1);
+  rb_thread_create(LMFAO_event_thread, NULL);
 }
